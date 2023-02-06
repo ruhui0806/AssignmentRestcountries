f612a4331e593033117c9e03c4381e74bac3c32a
ruhui0806 <ruhuiwen0608@gmail.com>
2023-02-06 11:59:36 +0200

    implement list of languages to the home page

--- integrify-assignement/src/components/CountryRow.js
+++ integrify-assignement/src/components/CountryRow.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const CountryRow = ({ country }) => {
+    const languages = country.languages;
+    console.log(languages);
+    const entries = languages
+        ? Object.keys(languages).map((key) => languages[key])
+        : [];
+    console.log('entries:', entries);
     return (
         <tr>
             <td>
@@ -15,11 +21,9 @@ const CountryRow = ({ country }) => {
             <td>{country.population}</td>
             <td>
                 <ul>
-                    {/* {Object.keys(country.languages).map((key) => (
-                        <li key={key}> {country.languages[key]} </li>
-                    ))} */}
-                    <li>1</li>
-                    <li>2</li>
+                    {entries.map((entry) => (
+                        <li key={entry}>{entry}</li>
+                    ))}
                 </ul>
             </td>
             <td>
