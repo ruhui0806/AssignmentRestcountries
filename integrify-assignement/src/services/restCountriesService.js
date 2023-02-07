ed713dcbe96c179e0aa55db6ff895f573936c11b
ruhui0806 <ruhuiwen0608@gmail.com>
2023-02-07 12:44:10 +0200

    eslint disable non anonymous default export

--- integrify-assignement/src/services/restCountriesService.js
+++ integrify-assignement/src/services/restCountriesService.js
@@ -9,4 +9,5 @@ const searchByName = async (name) => {
     const req = axios.get(`https://restcountries.com/v3.1/name/${name}`);
     return req.then((res) => res.data);
 };
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { getAll, searchByName };
